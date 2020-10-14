073b09f5fdb39113cfa94ff4c3378169164e3b7a
rohit <kumbharrohit13@gmail.com>
2020-10-14 13:38:27 +0530

    update

---
+++ OneToOne_Mapping/src/main/java/com/hb/entity/User.java
@@ -0,0 +1,33 @@
+package com.hb.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+	@Id
+	@GeneratedValue(generator = "user_id", strategy = GenerationType.AUTO)
+	@SequenceGenerator(name = "user_id", sequenceName = "user_id_generator",allocationSize = 1)
+	private int id;
+	private String firstName;
+	private String lastName;
+	@OneToOne
+	@Cascade(CascadeType.ALL)
+	private Address address;
+
+}
